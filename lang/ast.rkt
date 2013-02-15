66552bdc6c75b61cd9cefc751e527448e9db8a9d
Mark Hudnall <hudnall.mark@gmail.com>
2013-02-14 19:42:21 -0800

    typecheck def-decls, binds, expressions

--- lang/ast.rkt
+++ lang/ast.rkt
@@ -1,17 +1,14 @@
 #lang racket
-(provide all-defined-out)
-
 (define-for-syntax (grace-struct-syntax prefix stx) 
   (syntax-case stx ()
-    [(_ (struct-name (field ...)) ...)
+    [(_ (struct-name (field ...) struct-option ...) ...)
      (with-syntax ([(grace:struct ...) (map (lambda (id)
                                               (datum->syntax
                                                id
                                                (string->symbol
                                                 (format "~a:~a" prefix (syntax-e id)))))
                                             (syntax->list (syntax (struct-name ...))))])
-       (syntax (begin (define-struct grace:struct (field ...) #:transparent) ...
-                      (provide (struct-out grace:struct) ...))))]))
+       (syntax (begin (define-struct grace:struct (field ...) struct-option ... #:transparent) ...)))]))
 
 (define-syntax (define-grace-structs stx)
   (grace-struct-syntax "grace" stx))
@@ -20,13 +17,13 @@
   (grace-struct-syntax "grace:type" stx))
 
 (define-grace-structs
+  (number (value))
+  (str (value))
+  (identifier (value type))
+  
   (var-decl (name type value))
   (def-decl (name type value))
-  
   (bind (name value))
-  
-  (number (value))
-  (identifier (value type))
   
   (expression (op e1 e2))
   (method-call (name args))
@@ -35,16 +32,44 @@
   (member (parent name))
   (return (value))
   
-  (code-seq (code))
-  (str (value)))
+  (code-seq (code)))
+
+(define number-identifier (grace:identifier "Number" #f))
+(define string-identifier (grace:identifier "String" #f))
+(define boolean-identifier (grace:identifier "Boolean" #f))
+(define dynamic-identifier (grace:identifier "Dynamic" #f))
+(define list-identifier (grace:identifier "List" #f))
+
+(define number-other (grace:identifier "other" number-identifier))
+(define boolean-othter (grace:identifier "other" boolean-identifier))
+(define string-other (grace:identifier "other" string-identifier))
+(define top-other (grace:identifier "other" dynamic-identifier))
+(define list-other (grace:identifier "other" list-identifier))
+
+(define-struct grace:type (methods) #:transparent)
 
 (define-grace-types
-  (number ())
+  (method (name signature rtype))
   (string ())
   (list ())
   (boolean ())
   (dynamic ())
   (void ())
   (done ()))
-  
 
+(define grace:type:number (grace:type (list 
+                        (grace:type:method + (list number-other) number-identifier)
+                        (grace:type:method - (list number-other) number-identifier)
+                        (grace:type:method * (list number-other) number-identifier)
+                        (grace:type:method / (list number-other) number-identifier)
+                        (grace:type:method modulo (list number-other) number-identifier)
+                        (grace:type:method exp (list number-other) number-identifier)
+                        
+                        (grace:type:method equal? (list number-other) top-other)
+                        (grace:type:method 'not (list number-other) top-other)
+                        (grace:type:method < (list number-other) boolean-identifier)
+                        (grace:type:method > (list number-other) boolean-identifier)
+                        (grace:type:method <= (list number-other) boolean-identifier)
+                        (grace:type:method >= (list number-other) boolean-identifier))))
+
+(provide (all-defined-out))
--- lang/typecheck.rkt
+++ lang/typecheck.rkt
@@ -23,7 +23,7 @@
           ((empty? code-seq) (values t new-e))
           (else (type-check (grace:code-seq code-seq) new-e)))))
     ((grace:number value)
-     (values (grace:type:number) e))
+     (values grace:type:number e))
     ((grace:str value)
      (values (grace:type:string) e))
     ((grace:identifier value type)
@@ -37,18 +37,66 @@
                    [(type-type e2) (type-check type e)])
         (if (equal? value-type type-type)
             (begin 
+              (set-type e (grace:identifier-value name) value-type) 
+              (values value-type e))
+            (error `typecheck 
+                   "type annotation ~a on variable ~a does not match assignment type ~a" 
+                   (grace:identifier-value type)
+                   (grace:identifier-value name)
+                   value-type))))
+    ((grace:def-decl name type value)
+     (let-values ([(value-type e1) (type-check value e)]
+                   [(type-type e2) (type-check type e)])
+        (if (equal? value-type type-type)
+            (begin 
               (set-type e name value-type) 
               (values value-type e))
             (error `typecheck 
-                   "type annotation ~a on ~a does not match assignment type ~a" 
+                   "type annotation ~a on definition ~a does not match assignment type ~a" 
                    (grace:identifier-value type)
                    (grace:identifier-value name)
-                   value-type))
-        
-      )
-    )
+                   value-type))))
+    ((grace:bind name value)
+     (let-values ([(value-type e1) (type-check value e)]
+                  [(name-type _) (type-check name e)])
+       (if (equal? value-type name-type)
+           (values value-type e)
+           (error `typecheck 
+                   "type ~a of variable ~a does not match assignment type ~a" 
+                   name-type
+                   (grace:identifier-value name)
+                   value-type))))
+    ((grace:expression op e1 e2)
+     (let-values ([(e1-type env1) (type-check e1 e)]
+                  [(e2-type env2) (type-check e2 e)])
+       (let ((member-op (findf 
+                        (lambda (a) (equal? (grace:type:method-name a) op))
+                        (grace:type-methods e1-type))))
+         (if member-op
+             (let ((param-type
+                   (get-type e 
+                             (grace:identifier-value 
+                              (grace:identifier-type 
+                               (car (grace:type:method-signature member-op)))))))
+               (if (equal? param-type e2-type)
+                   (values (get-type e 
+                                     (grace:identifier-value 
+                                      (grace:type:method-rtype member-op))) 
+                           e)
+                   (error `typecheck
+                          "passed argument of type ~a to parameter of type ~a"
+                          e2-type 
+                          param-type)))
+             (error `typecheck
+                    "no such operator ~a in ~a"
+                    op e1-type)))))
+     
+     
+                 
+                 
   ))
 
+;; Removes all empty statements if (grace:code-seq? code-seq)
 (define (sanitize code-seq)
   (match code-seq
     ((grace:code-seq code)
@@ -57,22 +105,20 @@
 
 (define (typecheck thing)
   (let ((env (make-hash)))
-    (set-type env "Number" (grace:type:number))
+    (set-type env "Number" grace:type:number)
     (set-type env "String" (grace:type:string))
-    (set-type env "List" (grace:type:boolean))
+    (set-type env "List" (grace:type:list))
     (set-type env "Boolean" (grace:type:boolean))
-    (set-type env "Dynamic" (grace:type:boolean))
-    (set-type env "Void" (grace:type:boolean))
+    (set-type env "Dynamic" (grace:type:dynamic))
+    (set-type env "Void" (grace:type:void))
     (set-type env "Done" (grace:type:done))
     
-    (let-values ([(t e)(type-check (sanitize thing) env)])
-      (display t)
-      (display e)
-      e)
+    (set-type env "true" (grace:type:boolean))
+    (set-type env "false" (grace:type:boolean))
     
-    ))
+    (let-values ([(t e)(type-check (sanitize thing) env)])
+      e)))
       
-
 (provide typecheck)
   
     
