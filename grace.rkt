b12864e31cdcb37ddde036cd30e1a86442df5a07
Mark Hudnall <hudnall.mark@gmail.com>
2013-03-01 09:45:13 -0800

    testing file

--- grace.rkt
+++ grace.rkt
@@ -1,16 +1,30 @@
 #lang grace
-a.b.c()
-(a.b.c)()
+var a : String := "foo"
+var c : Number := 3
+var b : Number := 4 + 4 - 10 * 30 / 38 + c
 
-a.b().c
-(a.b()).c
+c := 45 + c
 
-a().c
-(a()).c
+method foo(s : String) -> Number {
+    return 4
+}
 
-a.b.c
-(a.b).c
+4 + 5 + 4 + foo("hello")
 
-a.b().d()().c
-a()()()
+var d : Dynamic := object {
+    var a : Number := 4
+    var b : Number := 7
+    method bar(s : String) -> Number {
+	    	self.a + self.b
+		self.a := 5
+		self.a	
+		a := 5
+		bar("hello")
+		self.bar("hello")
+		4
+    }
+    
+    var c : Number := self.bar("foo")
+}
 
+d.b
