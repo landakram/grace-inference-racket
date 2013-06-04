318bbe3138d73206b3b02c116fbac74781bb2b52
Nicholas Cho <nickcho2@gmail.com>
2013-06-04 10:23:07 -0700

    Clean up some formatting

--- grace/lang/lex.rkt
+++ grace/lang/lex.rkt
@@ -1,42 +1,79 @@
 #lang racket
+
 (require parser-tools/lex
          (prefix-in : parser-tools/lex-sre))
+
 (provide value-tokens op-tokens lex-this simple-grace-lexer)
 
-;value-tokens are tokens that contain a value.
-;op-tokens are names of all other tokens (including all keywords
-(define-tokens value-tokens (NUM STRING IDENTIFIER))
-(define-empty-tokens op-tokens (EOF 
-                                + - * / % ^ ++ ! && OR == < > <= >=
-                                = := : DOT \;
-                                //
-                                LBRACE RBRACE 
-                                LPAREN RPAREN 
-                                LBRACKET RBRACKET
-                                COMMA
-                                ARROW
-                                SEMICOLON
-                                NEWLINE
-                                OBJECT METHOD VAR TYPE IMPORT CLASS
-                                RETURN DEF INHERITS IS DIALECT IF THEN
-                                UNARY METHODCALL
-                                ))
+;; value-tokens are tokens that contain a value.
+(define-tokens value-tokens
+  (NUM STRING IDENTIFIER))
+
+
+;; op-tokens are names of all other tokens (including all keywords
+(define-empty-tokens op-tokens
+  (EOF
+   + - * / % ^ ++ ! && OR == < > <= >=
+   = := : DOT \;
+   //
+   LBRACE RBRACE
+   LPAREN RPAREN
+   LBRACKET RBRACKET
+   COMMA
+   ARROW
+   SEMICOLON
+   NEWLINE
+   OBJECT METHOD VAR TYPE IMPORT CLASS
+   RETURN DEF INHERITS IS DIALECT IF THEN
+   UNARY METHODCALL))
+
 
 (define-lex-abbrevs
   (CR #\015)
   (LF #\012)
   (FF #\014)
   (TAB #\011)
-  (my-whitespace (:or #\space TAB FF))
-  (my-newline (:or (:: CR) (:: LF) (:seq CR LF)))
-  (letter (:or (:/ "a" "z") (:/ #\A #\Z) ))
-  (digit (:/ #\0 #\9))
-  (number (:+ digit))
-  (string (:: "\"" (:* (:or letter digit "\\\"" (intersection punctuation (complement (char-set "\""))) whitespace)) "\""))
-  (comment (:: "//" (:* (:or letter digit punctuation (char-set "+-/*%><=") my-whitespace))))
-  (identifier (:: letter (:* (:or letter digit #\_ #\?))))
-  (keyword (:or "object" "method" "var" "type" "import" "class"
-                "return" "def" "inherits" "is" "dialect" "if" "then")))
+
+  (my-whitespace
+    (:or #\space TAB FF))
+
+  (my-newline
+    (:or (:: CR)
+         (:: LF)
+         (:seq CR LF)))
+
+  (letter
+    (:or (:/ "a" "z")
+         (:/ #\A #\Z) ))
+
+  (digit
+    (:/ #\0 #\9))
+
+  (number
+    (:+ digit))
+
+  (string
+    (:: "\"" (:* (:or letter
+                      digit
+                      "\\\""
+                      (intersection punctuation (complement (char-set "\"")))
+                      whitespace))
+        "\""))
+
+  (comment
+    (:: "//" (:* (:or letter
+                      digit
+                      punctuation
+                      (char-set "+-/*%><=")
+                      my-whitespace))))
+
+  (identifier
+    (:: letter (:* (:or letter digit #\_ #\?))))
+
+  (keyword
+    (:or "object" "method" "var" "type" "import" "class"
+         "return" "def" "inherits" "is" "dialect" "if" "then")))
+
 
 (define simple-grace-lexer
   (lexer-src-pos
@@ -74,8 +111,9 @@
    (keyword (string->symbol (string-upcase lexeme)))
    ; The call to call-with-input-string interprets the quoted string literally.
    ; Otherwise, the string is represented in racket as "\"theString\""
-   (string (token-STRING (call-with-input-string lexeme read))) 
+   (string (token-STRING (call-with-input-string lexeme read)))
    (identifier (token-IDENTIFIER (string->symbol lexeme)))
    (number (token-NUM (string->number lexeme)))))
 
-(define (lex-this lexer input) (lambda () (lexer input)))
+(define (lex-this lexer input)
+  (lambda () (lexer input)))
--- grace/lang/parse.rkt
+++ grace/lang/parse.rkt
@@ -1,26 +1,32 @@
 #lang racket
+
 (require parser-tools/yacc
          syntax/readerr
          "lex.rkt"
          "helpers.rkt"
          "ast.rkt")
+
 (provide parse)
 
 (define (parse src-name in)
   (parameterize ((current-source src-name))
     (simple-grace-parser (lambda () (simple-grace-lexer in)))))
- 
+
 (define simple-grace-parser
   (parser
    (start code-sequence)
+
    (end EOF)
    ;(suppress )
   ; (debug "errordump")
-   (error (lambda (a t v start end) 
+   (error (lambda (a t v start end)
             (raise-parse-error t v start end)))
+
    (src-pos)
+
    (tokens value-tokens op-tokens)
-   (precs 
+
+   (precs
     (left && OR)
     (left < <= > >= ==)
     (left - +)
@@ -31,68 +37,105 @@
     (nonassoc UNARY)
     (nonassoc LPAREN RPAREN)
     (left METHODCALL))
+
    (grammar
     (code-sequence ((_code-sequence) (at-src (grace:code-seq $1))))
-    (_code-sequence 
+
+    (_code-sequence
      ((code) (list $1))
      ((code _code-sequence) (cons $1 $2)))
+
     (code
      ((method-declaration) $1)
      ((statement) $1)
      ((NEWLINE) empty))
+
     (statement
      ((declaration NEWLINE) $1)
-     ((expression NEWLINE) $1)   
+     ((expression NEWLINE) $1)
      ((return NEWLINE) $1)
      ((if-then NEWLINE) $1)
      ((any := expression NEWLINE) (at-src (grace:bind $1 $3))))
-     
+
     (return
      ((RETURN) (at-src (grace:return "void")))
      ((RETURN expression) (at-src (grace:return $2))))
-    (declaration 
+
+    (declaration
      ((var-declaration) $1)
      ((def-declaration) $1)
      ((type-declaration) $1))
-    
+
     (type-declaration
-     ((TYPE identifier = LBRACE NEWLINE method-signatures RBRACE) (at-src (new grace:type:object% 
-                                                                       [internal-name (grace:identifier-value (syntax->datum $2))]
-                                                                       [methods $6]))))
+     ((TYPE identifier = LBRACE NEWLINE method-signatures RBRACE)
+      (at-src (new grace:type:object%
+                   [internal-name (grace:identifier-value (syntax->datum $2))]
+                   [methods $6]))))
+
     (method-signatures
-     ((method-name method-signature method-return-type NEWLINE) (list (new grace:type:method%
-                                                                                  [name (string->symbol (grace:identifier-value (syntax->datum $1)))]
-                                                                                  [signature $2]
-                                                                                  [rtype (syntax->datum $3)])))
-     ((method-name method-signature method-return-type NEWLINE method-signatures) (append (list (new grace:type:method%
-                                                                                  [name (string->symbol (grace:identifier-value (syntax->datum $1)))]
-                                                                                  [signature $2]
-                                                                                  [rtype (syntax->datum $3)])) $5)))
-    (var-declaration 
-     ((VAR identifier type-ref) (at-src (grace:var-decl $2 $3 #f)))
-     ((VAR identifier type-ref := expression) (at-src (grace:var-decl $2 $3 $5))))
+     ((method-name method-signature method-return-type NEWLINE)
+      (list (new grace:type:method%
+                 [name (string->symbol
+                         (grace:identifier-value (syntax->datum $1)))]
+                 [signature $2]
+                 [rtype (syntax->datum $3)])))
+
+     ((method-name method-signature method-return-type NEWLINE
+                   method-signatures)
+      (append (list (new grace:type:method%
+                         [name (string->symbol
+                                 (grace:identifier-value (syntax->datum $1)))]
+                         [signature $2]
+                         [rtype (syntax->datum $3)])) $5)))
+
+    (var-declaration
+     ((VAR identifier type-ref)
+      (at-src (grace:var-decl $2 $3 #f)))
+
+     ((VAR identifier type-ref := expression)
+      (at-src (grace:var-decl $2 $3 $5))))
+
     (type-ref
      ((: identifier) $2)
      (() #f))
+
     (def-declaration
-     ((DEF identifier type-ref = expression) (at-src (grace:def-decl $2 $3 $5))))
+     ((DEF identifier type-ref = expression)
+      (at-src (grace:def-decl $2 $3 $5))))
+
     (method-declaration
-     ((METHOD method-name method-return-type LBRACE method-body RBRACE NEWLINE) 
+     ((METHOD method-name method-return-type LBRACE method-body RBRACE NEWLINE)
       (at-src (grace:method $2 empty $5 $3)))
-     ((METHOD method-name method-signature method-return-type LBRACE method-body RBRACE NEWLINE) 
+
+     ((METHOD method-name method-signature method-return-type
+              LBRACE method-body RBRACE NEWLINE)
       (at-src (grace:method $2 $3 $6 $4))))
+
     (method-name
-     ; TODO: allow operators
+     ;@@@@@ TODO: allow operators @@@@@
      ((identifier) $1)
-     ((identifier :=) (at-src (grace:identifier (format "~a:=" (grace:identifier-value (syntax->datum $1))) #f))))
+
+     ((identifier :=)
+      (at-src
+        (grace:identifier
+          (format "~a:=" (grace:identifier-value (syntax->datum $1))) #f))))
+
     (method-signature
-     ((LPAREN RPAREN) empty) 
+     ((LPAREN RPAREN) empty)
      ((LPAREN signature-list RPAREN) $2))
+
     (signature-list
      ((identifier) (list $1))
-     ((IDENTIFIER : identifier) (list (at-src (grace:identifier (symbol->string $1) $3))))
+
+     ((IDENTIFIER : identifier)
+      (list (at-src (grace:identifier (symbol->string $1) $3))))
+
      ((identifier COMMA signature-list) (append (list $1) $3))
-     ((IDENTIFIER : identifier COMMA signature-list) (append (list (at-src (grace:identifier (symbol->string (quote $1)) $3))) $5)))
+
+     ((IDENTIFIER : identifier COMMA signature-list)
+      (append
+        (list (at-src (grace:identifier (symbol->string (quote $1)) $3))) $5)))
+
     (method-return-type
      ((ARROW identifier) $2)
      (() #f))
@@ -102,6 +145,7 @@
      ((NEWLINE) empty)
      ((NEWLINE method-body) $2)
      ((statement) (list $1)))
+
     (expression
      ((expression + expression) (at-src (grace:expression + $1 $3)))
      ((expression - expression) (at-src (grace:expression - $1 $3)))
@@ -109,58 +153,65 @@
      ((expression / expression) (at-src (grace:expression / $1 $3)))
      ((expression % expression) (at-src (grace:expression modulo $1 $3)))
      ((expression ^ expression) (at-src (grace:expression exp $1 $3)))
-     
+
      ((expression && expression) (at-src (grace:expression 'and $1 $3)))
      ((expression OR expression) (at-src (grace:expression 'or $1 $3)))
-     
+
      ((expression == expression) (at-src (grace:expression equal? $1 $3)))
      ((expression < expression) (at-src (grace:expression < $1 $3)))
      ((expression > expression) (at-src (grace:expression > $1 $3)))
      ((expression <= expression) (at-src (grace:expression <= $1 $3)))
      ((expression >= expression) (at-src (grace:expression >= $1 $3)))
-         
-     ((term) $1)
+
+     ((term) $1))
      ; multi-part method names
      ;((postfixsquare) $1)
-     )
+
     (parenthesis-expr ((LPAREN expression RPAREN) $2))
-    
-    (id-or-member 
+
+    (id-or-member
      ((identifier) $1)
+
      ((parenthesis-expr) $1)
-     ((id-or-member DOT identifier) (prec METHODCALL) (at-src (grace:member $1 $3))))
+
+     ((id-or-member DOT identifier)
+      (prec METHODCALL) (at-src (grace:member $1 $3))))
+
     (call
      ((id-or-member callrest) (at-src (grace:method-call $1 $2)))
      ((call callrest) (prec METHODCALL) (at-src (grace:method-call $1 $2))))
+
     (any-dot
      ((any DOT id-or-member) (at-src (grace:member $1 $3)))
      ((id-or-member) (prec UNARY) $1)
      ((call) (prec UNARY) $1))
+
     (any-call
      ((any-dot callrest) (at-src (grace:method-call $1 $2)))
      ((any-call callrest) (at-src (grace:method-call $1 $2))))
+
     (any
      ((any-call) $1)
      ((any-dot) $1))
 
-    (callrest 
+    (callrest
      ((LPAREN method-list RPAREN) $2)
      ((LPAREN RPAREN) empty))
-    
-    (method-list 
+
+    (method-list
      ((expression COMMA method-list) (append $1 $3))
      ((expression) (list $1)))
-    
+
     (term ((NUM) (at-src (grace:number $1)))
           ((STRING) (at-src (grace:str $1)))
           ((any) $1)
-          ((- term) 
-           (prec UNARY) 
-           (at-src (grace:method-call 
+          ((- term)
+           (prec UNARY)
+           (at-src (grace:method-call
                      (at-src (grace:member $2 -)) empty)))
-          ((+ term) 
-           (prec UNARY) 
-           (at-src (grace:method-call 
+          ((+ term)
+           (prec UNARY)
+           (at-src (grace:method-call
                      (at-src (grace:member $2 +)) empty)))
           ; matchcase
           ; catchcase
@@ -168,25 +219,32 @@
           ; dotypeterm
           ; block
           ; array
-    (identifier ((IDENTIFIER) (at-src (grace:identifier (symbol->string $1) #f))))
-    
-    (if-then ((IF LPAREN expression RPAREN THEN LPAREN if-body RPAREN) (at-src (grace:if-then $3 $7))))
-    
-    (object-decl ((OBJECT LBRACE object-body RBRACE) (at-src (grace:object $3)))
-                 ((OBJECT LBRACE RBRACE) (at-src (grace:object empty))))
-              ;extends, etc.
-    
-    (if-body      
+
+    (identifier
+      ((IDENTIFIER) (at-src (grace:identifier (symbol->string $1) #f))))
+
+    (if-then
+      ((IF LPAREN expression RPAREN THEN LPAREN if-body RPAREN)
+       (at-src (grace:if-then $3 $7))))
+
+    (object-decl
+      ((OBJECT LBRACE object-body RBRACE) (at-src (grace:object $3)))
+      ((OBJECT LBRACE RBRACE) (at-src (grace:object empty))))
+      ;@@@@@ TODO: extends, etc. @@@@@
+
+    (if-body
      ((_code-sequence) $1))
-    (object-body      
+
+    (object-body
      ((_code-sequence) $1))
+
     (possibly-newline
      ((NEWLINE) null)
-     (() null))
-   )))
+     (() null)))))
 
 
-;To test lexing and parsing without typechecking, copy your program into open-input-string 
+;To test lexing and parsing without typechecking, copy your program into
+;open-input-string
 ;(define (p in) (parse (object-name in) in))
 ;(define a (p (open-input-string "if (true) then (var z := 4 \n)\n")))
 ;(display a)
