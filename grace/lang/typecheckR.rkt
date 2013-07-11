e8e5349068cfa05d5d87438d1a85fafbec451cf5
Nicholas Cho <nickcho2@gmail.com>
2013-07-11 16:59:54 -0700

    Add notes on approach

--- grace/lang/typecheckR.rkt
+++ grace/lang/typecheckR.rkt
@@ -47,6 +47,50 @@
     ;; TODO: implement
     (void)))
 
+#|
+Notes:
+- In the type-env (with identifiers) may need to keep track of which are defs and which are vars.
+- As noted below, probably need an entry function and helper.
+
+## EXAMPLE PSEUDO_CODE ##
+function typecheck
+  build-environment() # This skips any inner blocks and just looks at the outermost scope
+  look-for-errors() # This will check the RHS of assignments, make sure no type errors, etc...
+
+function build-environment
+  push(typedef)to(scope-type-defs)
+  push(identifier)to(scope-type-env)
+
+function look-for-errors
+  match/case
+    var-decl ...
+    def-decl ...
+
+    ....
+
+    object-decl -> typecheck()  # This now recursively jumps into the block pushing a new env on the stack
+    if-then-else -> typecheck() # Similarly for other constructs that create a new scope
+## END PSEUDO_CODE ##
+
+
+MAYBE: (first sketch)
+* Need an entry function typecheck, then a recursive typecheck-helper.
+* The entry function will deal with the prelude and maybe all the above code needs to go into there.
+- It then should call some recursive helper that will first get the typedefs and build the type-env
+    for the outermost scope and then typecheck it, since nothing in a more inner scope should affect
+    the outermost scope. Then, the typechecker can recursively enter inner-more scopes, building on
+    top of the stack of typedefs and type-envs.
+- It should do this by running three functions: one will go through the list of syntax objects and
+    pick out the type-definitions and add it to the typedefs. The second will go through and look
+    for identifier bindings and add it to type-env. The last will 
+
+
+MAYBE MAYBE: (0.5th sketch)
+- The ast may have to be changed so that (void) isn't given in the list, as the typecheck function
+    may be expecting a Syntax type. We could add a newline struct in ast.rkt, or we could just
+    insert an empty Syntax object.
+|#
+
 
 
 
