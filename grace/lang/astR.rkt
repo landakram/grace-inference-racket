ad5c718acbdc8902497e710b14e7c8846cb8f894
Nicholas Cho <nickcho2@gmail.com>
2013-07-15 16:18:03 -0700

    Add prefab option to ast nodes to pretty print in macro stepper

--- grace/lang/astR.rkt
+++ grace/lang/astR.rkt
@@ -6,94 +6,113 @@
 (struct: method-type
   ([name : String]
    [signature : (Listof String)]
-   [rtype : String]))
+   [rtype : String])
+  #:prefab)
 
 (define-type type-name
   (U (Syntaxof grace:identifier) #f))
 
 (struct: grace:number
-  ([value : Number]))
+  ([value : Number])
+  #:prefab)
 
 (struct: grace:str
-  ([value : String]))
+  ([value : String])
+  #:prefab)
 
 (struct: grace:identifier
   ([value : String]
-   [type  : type-name]))
+   [type  : type-name])
+  #:prefab)
 
 (struct: grace:method-def
   ([name      : (Syntaxof grace:identifier)]
    [signature : (Listof (Syntaxof grace:identifier))]
-   [rtype     : (Syntaxof grace:identifier)]))
+   [rtype     : (Syntaxof grace:identifier)])
+  #:prefab)
 
 (struct: grace:type-def
   ([name    : (Syntaxof grace:identifier)]
-   [methods : (Listof grace:method-def)]))
+   [methods : (Listof grace:method-def)])
+  #:prefab)
 
 (struct: grace:var-decl
   ([name  : (Syntaxof grace:identifier)]
    [type  : type-name]
-   [value : Any]))
+   [value : Any])
+  #:prefab)
 
 (struct: grace:def-decl
   ([name  : (Syntaxof grace:identifier)]
    [type  : type-name]
    ;; TODO: possibly fix type of value
-   [value : Any]))
+   [value : Any])
+  #:prefab)
 
 (struct: grace:bind
   ([name  : (Syntaxof grace:identifier)]
    ;; TODO: possibly fix type of value
-   [value : Any]))
+   [value : Any])
+  #:prefab)
 
 (struct: grace:expression
   ([op  : Symbol]
    ;; TODO: Fix type of operands
    [lhs : Any]
-   [rhs : Any]))
+   [rhs : Any])
+  #:prefab)
 
 (struct: grace:method-call
   ([name : (U (Syntaxof grace:identifier) (Syntaxof grace:member))]
    ;; TODO: Fix type of args
-   [args : (Listof Any)]))
+   [args : (Listof Any)])
+  #:prefab)
 
 (struct: grace:object
-  ([body : (Listof (Syntaxof Any))]))
+  ([body : (Listof (Syntaxof Any))])
+  #:prefab)
 
 (struct: grace:method
   ([name      : (Syntaxof grace:identifier)]
    [signature : (Listof (Syntaxof grace:identifier))]
    ;; TODO: Fix type of body
    [body : (Listof (Syntaxof Any))]
-   [type : type-name]))
+   [type : type-name])
+  #:prefab)
 
 (struct: grace:member
   ([parent : (U (Syntaxof grace:identifier) (Syntaxof grace:member))]
-   [name   : (Syntaxof grace:identifier)]))
+   [name   : (Syntaxof grace:identifier)])
+  #:prefab)
 
 (struct: grace:return
   ;; TODO: Fix type of value, maybe grace:expression or identifier.
-  ([value : Any]))
+  ([value : Any])
+  #:prefab)
 
 (struct: grace:if-then-else
   ;; TODO: Type of check definitely needs fixing.
   ([check : (U (Syntaxof grace:identifier) (Syntaxof grace:expression) (Syntaxof grace:member) (Syntaxof grace:method-call))]
    [tbody : (Listof (Syntaxof Any))]
-   [ebody : (Listof (Syntaxof Any))]))
+   [ebody : (Listof (Syntaxof Any))])
+  #:prefab)
 
 (struct: grace:class-decl
   ([name       : (Syntaxof grace:identifier)]
    [param-name : (Syntaxof grace:identifier)]
    ;; TODO: Fix, maybe grace:identifier?
    [signature  : (Listof Any)]
-   [body       : (Listof Any)]))
+   [body       : (Listof Any)])
+  #:prefab)
 
 ;(struct: grace:code-seq
 ;  ([code : Any]))
 (struct: grace:code-seq
-  ([code : (Listof (Syntaxof Any))]))
+  ([code : (Listof (Syntaxof Any))])
+  #:prefab)
 
 (struct: grace:newline
-  ())
+  ()
+  #:prefab)
 
 (provide (all-defined-out))
